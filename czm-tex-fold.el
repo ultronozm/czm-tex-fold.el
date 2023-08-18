a93fad12d29e35d2c119b68983b657a7c14df193
Paul Nelson <ultrono@gmail.com>
2023-08-18 08:35:24 +0200

    Update czm-tex-fold.el

--- czm-tex-fold.el
+++ czm-tex-fold.el
@@ -3,9 +3,9 @@
 ;; Copyright (C) 2023  Paul D. Nelson
 
 ;; Author: Paul D. Nelson <nelson.paul.david@gmail.com>
-;; Version: 0.1
+;; Version: 0.0
 ;; URL: https://github.com/ultronozm/czm-tex-fold.el
-;; Package-Requires: ((emacs "29.1") (czm-tex-util "0.1"))
+;; Package-Requires: ((emacs "29.1") (czm-tex-util "0.0"))
 ;; Keywords: tex
 
 ;; This program is free software; you can redistribute it and/or modify
