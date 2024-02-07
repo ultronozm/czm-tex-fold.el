587fe1c3892fb493ef4e435da795f3cb32caa508
Paul Nelson <ultrono@gmail.com>
2024-02-07 13:39:58 +0100

    make "\begin{fact}" foldable

--- czm-tex-fold.el
+++ czm-tex-fold.el
@@ -160,7 +160,7 @@
     ((czm-tex-fold-standard-display . "□")
      ("proof"))
     ((czm-tex-fold-standard-display . "◼")
-     ("abstract" "lemma" "exercise" "example" "proposition" "corollary" "remark" "definition" "theorem" "proof" "conjecture" "notation" "terminology" "note" "problem" "ass" "acknowledgment" "algorithm" "question" "answer" "claim" "conclusion" "criterion" "summary" "thm" "prop" "rem" "cor" "lem" "lemmy" "def" "defn" "ex" "exer" "conj" "not" "term" "prob" "ques" "ans" "conc" "crit" "sum"))
+     ("abstract" "lemma" "exercise" "example" "proposition" "corollary" "remark" "definition" "theorem" "proof" "conjecture" "notation" "terminology" "fact" "note" "problem" "ass" "acknowledgment" "algorithm" "question" "answer" "claim" "conclusion" "criterion" "summary" "thm" "prop" "rem" "cor" "lem" "lemmy" "def" "defn" "ex" "exer" "conj" "not" "term" "prob" "ques" "ans" "conc" "crit" "sum"))
     ((czm-tex-fold-block-display . "◼")
      ("block")))
   "List of specifications for `czm-tex-fold-begin-display'.
